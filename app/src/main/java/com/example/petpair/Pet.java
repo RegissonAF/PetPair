a99f503cecdb9a501b41313e32e5bf721910bdd3
Regisson Andre de Lara Filho <99519216+RegissonAF@users.noreply.github.com>
2024-06-09 21:45:03 -0300

    Criando as classes para o Usuario e o Pet.

--- app/src/main/java/com/example/petpair/Pet.java
+++ app/src/main/java/com/example/petpair/Pet.java
@@ -1,4 +1,70 @@
 package com.example.petpair;
 
 public class Pet {
+    private String nome;
+    private String raca;
+    private String ownerId;
+    private String id;
+    private int sexo;
+    private int idade;
+
+    public Pet() {
+    }
+
+    public Pet(String nome, String raca, String ownerId, String id, int sexo, int idade) {
+        this.nome = nome;
+        this.raca = raca;
+        this.ownerId = ownerId;
+        this.id = id;
+        this.sexo = sexo;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+
+    public String getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(String ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(int sexo) {
+        this.sexo = sexo;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
 }
--- app/src/main/java/com/example/petpair/User.java
+++ app/src/main/java/com/example/petpair/User.java
@@ -1,4 +1,111 @@
 package com.example.petpair;
 
 public class User {
+    private String nome;
+    private int idade;
+    private int sexo;
+    private String cep;
+    private String bairro;
+    private String cidade;
+    private String estado;
+    private String pais;
+    private String rua;
+    private String petId;
+
+    public User() {
+    }
+
+    public User(String nome, int idade, int sexo, String cep, String bairro, String cidade, String estado, String pais, String rua, String petId) {
+        this.nome = nome;
+        this.idade = idade;
+        this.sexo = sexo;
+        this.cep = cep;
+        this.bairro = bairro;
+        this.cidade = cidade;
+        this.estado = estado;
+        this.pais = pais;
+        this.rua = rua;
+        this.petId = petId;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(int sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getPetId() {
+        return petId;
+    }
+
+    public void setPetId(String petId) {
+        this.petId = petId;
+    }
 }
+
